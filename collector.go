e67ccbf56bc939f63551d8f3a1f99d4fb77555ba
马犇 <benma@angejia.com>
2016-08-26 16:49:00 +0800

    finish

--- collector.go
+++ collector.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	_ "net"
+	"net"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
-	_ "github.com/influxdata/telegraf/plugins/parsers"
 	"github.com/jinzhu/configor"
 
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -27,21 +28,146 @@ var Config = struct {
 	}
 }{}
 
+type collectItem struct {
+	Table     string
+	Sql       string
+	Frequency uint
+}
+
 var Sqls = struct {
-	Sql []struct {
-		Sql       string
-		Frequency uint
-	}
+	Sql []collectItem
 }{}
 
-var wg sync.WaitGroup
+var Ctx = struct {
+	MysqlConn *sql.DB
+	Wg        sync.WaitGroup
+	TcpConn   net.Conn
+}{}
 
-func startTimer(sql string, frequency uint) {
+/**
+ ************ MySQL *************
+ * {{{
+ */
+func initMySQL() error {
+	var err error = nil
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/",
+		Config.DB.Username,
+		Config.DB.Password,
+		Config.DB.Host,
+		Config.DB.Port,
+	)
+	Ctx.MysqlConn, err = sql.Open("mysql", connStr)
+	if err != nil {
+		return err
+	}
+
+	err = Ctx.MysqlConn.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func formatSql(s string, frequency uint) string {
+	end_time := time.Now().Format("2006-01-02 15:04:05")
+	start_time := time.Unix(int64(time.Now().Unix()-int64(frequency)), 0).Format("2006-01-02 15:04:05")
+	s = strings.Replace(s, "{START_TIME}", start_time, -1)
+	s = strings.Replace(s, "{END_TIME}", end_time, -1)
+	return s
+}
+
+/**
+ * }}}
+ ************ MySQL *************
+ */
+
+/**
+ ************ Telegraf *************
+ * {{{
+ */
+
+func initTcpConn() error {
+	var err error = nil
+	Ctx.TcpConn, err = net.Dial("tcp", fmt.Sprintf("%s:%d", Config.Server.Host, Config.Server.Port))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func send(msg string) error {
+	if len(msg) < 1 {
+		return nil
+	}
+	if msg[len(msg)-1] != '\n' {
+		msg += "\n"
+	}
+	_, err := Ctx.TcpConn.Write([]byte(msg))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/**
+ * }}}
+ ************ Telegraf *************
+ */
+
+func executeQuery(s, table string) {
+	rows, err := Ctx.MysqlConn.Query(s)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Query error ", err.Error())
+		return
+	}
+
+	columns, err := rows.Columns()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Get columns error ", err.Error())
+		return
+	}
+
+	values := make([]sql.RawBytes, len(columns))
+
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	if rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+		}
+
+		var value string
+		msg := fmt.Sprintf("%s ", table)
+		for i, col := range values {
+			if i > 0 {
+				msg += ","
+			}
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			msg += columns[i] + "=" + value
+		}
+
+		err := send(msg)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+	}
+
+}
+
+func startTimer(item collectItem) {
 	for {
-		fmt.Println(sql)
-		//now := time.Now()
-		//next := now.Add(time.Second * time.Duration(frequency))
-		t := time.NewTimer(time.Second * time.Duration(frequency))
+		newsql := formatSql(item.Sql, item.Frequency)
+
+		fmt.Println(newsql)
+		executeQuery(newsql, item.Table)
+		t := time.NewTimer(time.Second * time.Duration(item.Frequency))
 		<-t.C
 	}
 
@@ -49,19 +175,19 @@ func startTimer(sql string, frequency uint) {
 
 func loop() {
 	for _, s := range Sqls.Sql {
-		wg.Add(1)
-		go startTimer(s.Sql, s.Frequency)
+		Ctx.Wg.Add(1)
+		go startTimer(s)
 	}
 }
 
 func main() {
 	if err := configor.Load(&Config, "config.json"); err != nil {
-		fmt.Fprint(os.Stderr, "Failed to load config.json")
+		fmt.Fprintln(os.Stderr, "Failed to load config.json")
 		os.Exit(1)
 	}
 
 	if err := configor.Load(&Sqls, "sql.json"); err != nil {
-		fmt.Fprint(os.Stderr, "Failed to load sql.json", err.Error())
+		fmt.Fprintln(os.Stderr, "Failed to load sql.json", err.Error())
 		os.Exit(1)
 	}
 
@@ -69,6 +195,21 @@ func main() {
 		fmt.Fprintln(os.Stdout, "No task to work")
 		os.Exit(0)
 	}
+
+	if err := initMySQL(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed initialize MySQL ", err.Error())
+		os.Exit(1)
+	}
+
+	if err := initTcpConn(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to connect telegraf", err.Error())
+		os.Exit(1)
+	}
+	defer func() {
+		Ctx.MysqlConn.Close()
+		Ctx.TcpConn.Close()
+	}()
+
 	loop()
-	wg.Wait()
+	Ctx.Wg.Wait()
 }
